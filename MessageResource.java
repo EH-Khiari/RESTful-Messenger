fa98ead15a8705a593949f1df0dc4bb7ad3c4771
El Hebri Khiari <hebri.khiari@gmail.com>
2015-08-21 14:41:24 -0400

    MessageResource tempalte file

---
+++ MessageResource.java
@@ -0,0 +1,21 @@
+package org.koushik.javabrains.Qlick_messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import java.util.ArrayList;
+
+@Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Messenger_resource {
+
+}
