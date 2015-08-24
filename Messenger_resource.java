343b32357669ec2adabdf61bce44f532d1b43a61
El Hebri Khiari <hebri.khiari@gmail.com>
2015-08-23 21:44:08 -0400

    Update Messenger_resource.java

--- Messenger_resource.java
+++ Messenger_resource.java
@@ -16,6 +16,8 @@ import org.koushik.javabrains.Qlick_messenger.service.Messenger_service;
 import java.util.ArrayList;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 
 public class Messenger_resource {
 	
@@ -28,9 +30,22 @@ public class Messenger_resource {
 	}
 	
 	@GET
-	public Message_model getMessage(long id)
+	@Path("/{messageId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage(@PathParam("messageId") long id)
 	{
-		return message_service.getMessage(id);
+		if (id <= 0) return "Invalid ID request";
+		
+		Message_model rec_message = message_service.getMessage(id);
+		
+		String content  = rec_message.getContent();
+		boolean isPalindrome = isPalindrome(content);
+		
+		if (isPalindrome) 
+			return "Received message: ``" + content + "`` is a palindomre.";
+			
+		else
+			return "Received message: ``" + content + "`` is not a palindomre.";
 	}
 	
 	@GET 
@@ -41,14 +56,30 @@ public class Messenger_resource {
 	
 	
 	@PUT
-	public Message_model updateMessage(long id, Message_model new_message)
+	@Path("/{messageId}")
+	public Message_model updateMessage(@PathParam("messageId") long id, Message_model new_message)
 	{
+		new_message.setId(id);
 		return message_service.updateMessage(new_message);
 	}
 	
 	@DELETE
-	public void deleteMessage(long id)
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id)
 	{
 		message_service.deleteMessage(id);
+	}
+	
+	public boolean isPalindrome(String string)
+	{
+		String no_spaces = "";
+		for (int i = 0; i < string.length(); i++)
+		{
+			if (string.charAt(i) != ' ') no_spaces += string.charAt(i);
+		}
+		
+		String reversed = new StringBuffer(no_spaces).reverse().toString();
+		
+		return no_spaces.equals(reversed);
 	}
 }
